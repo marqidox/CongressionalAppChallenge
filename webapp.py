01c104b08387132011482c0de0e465980a1b9ada
Jada Ogueh <jadaogueh@jmb.attlocal.net>
2024-10-23 18:47:46 -0500

    add project files to github

--- webapp.py
+++ webapp.py
@@ -16,41 +16,51 @@ from datetime import datetime
 mp_drawing = mp.solutions.drawing_utils # Draw the detections from the model to the screen
 mp_holistic = mp.solutions.holistic # Mediapipe Solutions holistic model
 
-finished = True
-lock = threading.Lock()
-student_emotion_container = {'happy': 1, 'bored': 1, "confused": 1, 'sad': 1}
-class Student:
+st.set_page_config(
+    page_title="Job Interview Simulator",
+    layout="wide"
+)
+
+c1, c2 = st.columns(2)
+
+st.title("Job Interview Simulator")
+st.write("This tool will track your body language when responding to AI interviewer. This real-time feedback will train for the real thing.")
+st.image(use_column_width="always",image="pexels-thisisengineering-3861969.jpg")
+st.header("Begin the Simulator Below")
+
+@st.cache_resource
+class Applicant:
     def __init__(self):
         self.detected_emotion = None
 
-    def update_emotion(self, s_class):
-        self.detected_emotion = s_class
-        return self.send_message_to_student()
+    def update_emotion(self, a_class):
+        self.detected_emotion = a_class
+        return self.send_message_to_applicant()
 
     def get_emotion(self):
         return self.detected_emotion
 
-    def send_message_to_student(self): # works
-        # could make a bank of messages and select randomly from it
+    def send_message_to_applicant(self):
         if self.detected_emotion == "happy":
-            return "Give a thumbs up in the chat!"
+            return "It is good that you are appear excited and eager--interviewers want to see interest!"
         if self.detected_emotion == "bored":
-            return "Stay engaged by being active! Or, ask for a break."
+            return "Liven up! Even if this interview may be boring, or this job is not exactly what you want, you must appear engaged!"
         if self.detected_emotion == "confused":
-            return "Raise your hand to ask the teacher a question."
+            return "Don't forget you are evaluating this company's fit for you! If you need clarification, wait for a good stopping point, then ask! Asking shows interest and curiosity."
         if self.detected_emotion == 'sad':
-            return "Talk to someone to process your emotions."
+            return "This process may be difficult, but chin up! Negative emotion could be misconstrued as disinterest or dissatisification with job details."
 
-st.title("Body Language Detection for Online Learning")
-st.header("Student Interface")
-#st.toggle("Activate Body Language Detector")
 with open(r"body_language_model_official_gbc3.pkl","rb") as f:
     model = pickle.load(f)
 
+finished=True
+lock = threading.Lock()
+job_applicant_container = {'happy': 1, 'bored': 1, "confused": 1, 'sad': 1}
+
 def callback(frame):
-    global student_emotion_container
+    global job_applicant_container
     img = frame.to_ndarray(format="bgr24")
-    new_student = Student()
+    new_applicant = Applicant()
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         img.flags.writeable = False
@@ -68,7 +78,7 @@ def callback(frame):
         row = pose_row + face_row
         x = pd.DataFrame([row])
         body_language_class = model.predict(x)[0]  # first value of the predict array
-        msg = new_student.update_emotion(body_language_class.lower())
+        msg = new_applicant.update_emotion(body_language_class.lower())
         y_max = int(max([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
         y_min = int(min([landmark.y for landmark in results.face_landmarks.landmark]) * 480)
         x_max = int(max([landmark.x for landmark in results.face_landmarks.landmark]) * 640)
@@ -81,52 +91,47 @@ def callback(frame):
         cv2.rectangle(img, (2, 2), (0 + len(msg) * 15 + 5, 33), (255, 255, 255), -1)
         cv2.putText(img, msg, (9, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
     with lock:
-        student_emotion_container[body_language_class.lower()] += 1
+        job_applicant_container[body_language_class.lower()] += 1
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
+with c1:
+    ctx = webrtc_streamer(key="example", video_frame_callback=callback, rtc_configuration={
+            "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+        })
+    st.header("This is the mock interviewer.")
 
-ctx = webrtc_streamer(key="example", video_frame_callback=callback, rtc_configuration={
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    })
-st.header("This is the teacher interface.")
-st.write("This is what will appear on a teacher's screen in full deployment.")
-stutter = st.empty()
-while ctx.state.playing:
-    with lock:
-        finished = False
-    with stutter.container():
-        now = datetime.now().astimezone().strftime("%I:%M%p (%Z)")
-        st.write("The time is: " + str(now))
+    stutter = st.empty()
+    while ctx.state.playing:
         with lock:
-            data = student_emotion_container
-            labels = list(student_emotion_container.keys())
-            counts = list(student_emotion_container.values())
-        pe_e = max(data, key=data.get)
-        st.write(f"Majority of your students are {pe_e}.")
-        if pe_e == "happy":
-            st.write("They are very engaged! Keep on teaching!")
-        if pe_e == "bored":
-            st.write("Try different engaging strategies, such as calling on students or fun quizzes.")
-        if pe_e == "confused":
-            st.write("Try explaining the concepts in a different way.")
-        if pe_e == "sad":
-            st.write("Check in on your students' mental and physical health!")
-        fig, ax = plt.subplots()
-        ax.pie(counts, labels=labels, autopct='%.2f')
-        ax.axis('equal')
-        st.pyplot(fig)
-        with open("dump.txt", 'w') as file:
-            file.write(pe_e)
-        time.sleep(5)
+            finished = False
+        with stutter.container():
+            with lock:
+                data = job_applicant_container
+                labels = list(job_applicant_container.keys())
+                counts = list(job_applicant_container.values())
+            pe_e = max(data, key=data.get)
+            st.write(f"Your body language mostly indicates you are {pe_e}.")
+            fig, ax = plt.subplots()
+            ax.pie(counts, labels=labels, autopct='%.2f')
+            ax.axis('equal')
+            st.pyplot(fig)
+            with open("dump.txt", 'w') as file:
+                file.write(pe_e)
+            time.sleep(5)
+with c2:
+    st.write("AI Here")
+    pass
 
 with open("dump.txt") as file:
     main_emotion = file.read()
-def generate_report_for_teacher(majority_emotion, subject, grade):
+
+@st.cache_resource
+def generate_advice_for_applicant(majority_emotion, occupation):
     response = requests.post(
         url="https://openrouter.ai/api/v1/chat/completions",
         data=json.dumps({
             "models": ["nousresearch/nous-capybara-7b", "mistralai/mistral-7b-instruct","huggingfaceh4/zephyr-7b-beta"],
-            "messages": [{"role": "user", "content": f"You are teaching the subject {subject} to students in {grade} online. Given that the emotion most commonly detected from your students was {majority_emotion}, generate advice for better instruction and teaching."}],
+            "messages": [{"role": "user", "content": f"You are applying to be a {occupation}. Having just completed your job interview, the body language most commonly detected was {majority_emotion}. Based on this, assuming the POV of a job interviewer, generate advice for how to improve body language during an interview to maximize chances of being hired."}],
             "route": 'fallback'
         }),
         headers={"Authorization": f"Bearer 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"}
@@ -137,17 +142,12 @@ def generate_report_for_teacher(majority_emotion, subject, grade):
 
 # program started and captured new data
 if finished:
-    st.write(f'Overall, most of your students were {main_emotion}.')
     result = st.subheader("Get Feedback from Chatbot")
-    with st.form("teacher_feedback"):
+    with st.form("applicant_feedback"):
         st.write("Please fill out the requested fields.")
-        subject = st.text_input("What subject do you teach?")
-        grade = st.selectbox(
-            "What grade level do you teach?",
-            ("elementary", "middle school", 'high school', 'undergraduate college', 'graduate school')
-        )
+        job = st.text_input("What job were you applying for? ex. software engineer")
         submitted = st.form_submit_button("Submit")
     if submitted:
-        cnt = generate_report_for_teacher(main_emotion, subject, grade)
+        cnt = generate_advice_for_applicant(main_emotion, job)
         st.write(cnt)
 
