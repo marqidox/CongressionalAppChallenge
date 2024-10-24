d170b69a711c4295c254825abd3e30467b5127ba
jada <67612300+marqidox@users.noreply.github.com>
2024-10-24 15:56:22 -0500

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -158,7 +158,7 @@ if submitted:
     with video.container():
         c1, c2, c3 = st.columns(3)
         with c1:
-            
+            pass
         with c2:
             if st.session_state.t1:
                 st.image("blackmaninterviewer1.jpg")
